6c976e34a64357e768cfcdda4182b4ebcd117aa3
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-13 01:35:05 +0530

    4rd time file changes

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
-// import About from '../../components/mydetails/Mydetails'
 import Mydetails from '../../components/mydetails/Mydetails'
 import Skill from '../../components/skills/Skill'
 import Education from '../../components/education/Education'
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:8800',
+//     },
+//   },
+
+  
+// })
+/////////////////////////
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';  // Explicitly import the 'path' module
 
 export default defineConfig({
   plugins: [react()],
-  root: '/opt/build/repo',
-  // server: {
-  //   proxy: {
-  //     '/api': 'http://localhost:8800',
-  //   },
-  // },
   resolve: {
     alias: {
       '/src': path.resolve(__dirname, 'src'),
     },
   },
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8800',
+    },
+  },
+});
 
