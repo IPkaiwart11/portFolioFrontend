a0b523eb2eb7a7903b7a553b8fe6a640c6504b7b
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-13 01:48:38 +0530

    4rd time file changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Navbar from '/src/components/navbar/Navbar';
-import Footer from '/src/components/footer/Footer';
-import Slider from '/src/components/slider/Slider';
-import Fontview from '/src/components/frontview/fontview';
+import Navbar from '/src/components/navbar/Navbar.jsx';
+import Footer from '/src/components/footer/Footer.jsx';
+import Slider from '/src/components/slider/Slider.jsx';
+import Fontview from '/src/components/frontview/Fontview.jsx';
 // import Navbar from '../../components/navbar/Navbar';
 // import Footer from '../../components/footer/Footer';
 // import Fontview from '../../components/frontview/fontview';
