726f816cfc6a3cccab708868dbaaafb7bee75100
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-13 01:08:17 +0530

    3rd time file changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8800',
-    },
-  },
+  root: '/opt/build/repo',
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:8800',
+  //   },
+  // },
   resolve: {
     alias: {
       '/src': 'opt/build/repo/src',
