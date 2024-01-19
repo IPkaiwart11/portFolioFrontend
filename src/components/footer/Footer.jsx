2092b7296f0aa804b01ca8c5607f1f3414866ce6
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-19 22:22:54 +0530

    target atrri

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import GitHubIcon from '@mui/icons-material/GitHub';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import { blue } from '@mui/material/colors';
 import './footer.scss'
 import { NavLink } from 'react-router-dom';
@@ -9,9 +10,9 @@ export default function Footer() {
                return (
                               <>
                                              <div className="footerContainer">
-                                                           <NavLink> <div><LinkedInIcon sx={{ fontSize: 40, color: blue[900] }} /></div></NavLink>
-                                                           <NavLink>   <div><FacebookIcon sx={{ fontSize: 40, color: blue[900] }} /></div></NavLink>
-                                                           <NavLink>   <div><GitHubIcon sx={{ fontSize: 40, color: blue[900] }} /></div></NavLink>
+                                                           <NavLink> <a href="www.linkedin.com/in/ipkaiwart" target='_blank'><div><LinkedInIcon sx={{ fontSize: 40, color: blue[900] }} /></div></a></NavLink>
+                                                           {/* <NavLink>   <div><FacebookIcon sx={{ fontSize: 40, color: blue[900] }} /></div></NavLink> */}
+                                                           <NavLink> <a href="https://www.instagram.com/ip_kaiwart/" target='_blank'>  <div><InstagramIcon sx={{ fontSize: 40, color: blue[900] }} /></div></a></NavLink>
                                              </div>
                               </>
                )
