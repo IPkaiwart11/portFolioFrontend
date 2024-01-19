3488d59c0226dd2d6841de7b3d738e0475694af2
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-20 00:58:42 +0530

    target atrri

--- src/components/contact/Contactme.jsx
+++ src/components/contact/Contactme.jsx
@@ -47,23 +47,23 @@ export default function Contactme() {
     <form onSubmit={handleSubmit}>
         <div className="field">
         <label htmlFor="name">Enter Your Full Name:</label>
-               <input type="text" onChange={handleChange} htmlFor='name' name='fullname' placeholder='Ex- Mukesh kaiwart' />
+               <input type="text" onChange={handleChange} htmlFor='name' name='fullname' placeholder='Ex- Mukesh kaiwart' required/>
         </div>
         <div className="field">
         <label htmlFor="email">Email:</label>
-              <input type="email" htmlFor='email' onChange={handleChange} name='email' placeholder='abc@gmail.com'/>
+              <input type="email" htmlFor='email' onChange={handleChange} name='email' placeholder='abc@gmail.com' required/>
               </div>
         <div className="field">
         <label htmlFor="Country">Country</label>
        
-       <select name="country" onChange={handleChange} id="country" value={formData.country}>
+       <select name="country" onChange={handleChange} id="country" value={formData.country} required>
         <option value="india">India</option>
         <option value="usa">USA</option>
         <option value="australia">Australia</option>
        </select>
         </div>
         <div className='field'>
-        <textarea name="message" onChange={handleChange} id="" rows="6"></textarea>
+        <textarea name="message" onChange={handleChange} id="" rows="6" required></textarea>
         </div>
         <div className='field'>
         <input type="submit" placeholder='submit'/>
