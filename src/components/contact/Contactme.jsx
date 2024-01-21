140aef2960a4ece6e5fd379a07a705805d81debd
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-01-21 21:53:15 +0530

    target atrri

--- src/components/contact/Contactme.jsx
+++ src/components/contact/Contactme.jsx
@@ -19,7 +19,9 @@ export default function Contactme() {
   const handleSubmit = async(e)=>{
    e.preventDefault();
    try{
-    const res = await axios.post('https://portfoliobackend-kunp.onrender.com/api/user',formData);
+    const res = await axios.post('https://portfoliobackend-kunp.onrender.com/api/user',formData,{
+      withCredentials: true,
+    });
 
     console.log('Server response:',res.data);
     alert('successful! Thank you for your connection');
