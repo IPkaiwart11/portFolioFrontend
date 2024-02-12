1346baae3f52ec7d014eebb9489e9aa9cae1c47b
IPkaiwart11 <kaiwartindrapal@gmail.com>
2024-02-13 00:22:59 +0530

    modify project

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,82 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import './slider.scss';
-
-export default function Slider() {
-  const [slideIndex, setSlideIndex] = useState(1);
-
-  useEffect(() => {
-    showSlides(slideIndex);
-  }, [slideIndex]);
 
 
-function plusSlides(n) {
-               let newIndex = slideIndex + n;
-             
-               // If the new index is greater than the number of slides, go to the first slide
-               if (newIndex > 3) {
-                 setSlideIndex(1);
-               }
-               // If the new index is less than 1, go to the last slide
-               else if (newIndex < 1) {
-                 setSlideIndex(3);
-               } else {
-                 setSlideIndex(newIndex);
-               }
-             }
-             
+import React, { useState } from 'react';
+import './slider.scss'; // Import CSS file with styles
 
-  function showSlides() {
-    
-    let slides = document.getElementsByClassName("mySlides");
-//     if (n > slides.length) {
-//       setSlideIndex(1);
-//     }
-//     if (n < 1) {
-//       setSlideIndex(slides.length);
-//     }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
+function Slider() {
+  // State to manage the active filter
+  const [filter, setFilter] = useState('all');
 
-    slides[slideIndex - 1].style.display = "block";
-  }
+  // Function to handle filter selection
+  const handleFilter = (filterName) => {
+    setFilter(filterName);
+  };
 
   return (
-    <>
-      <div className="slideshow-container">
-        <div className="mySlides">
-          <div className="numbertext">1 / 3</div>
-          <img
-            src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg"
-            style={{ width: '100%' }}
-            alt="Slide 1"
-          />
-          {/* <div className="text">car 1</div> */}
-        </div>
+    <div className="main">
+      <h1>My Gallery</h1><br /><br />
+    
+      {/* <h2></h2> */}
 
-        <div className="mySlides">
-          <div className="numbertext">2 / 3</div>
-          <img
-            src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            style={{ width: '100%' }}
-            alt="Slide 2"
-          />
-          {/* <div className="text">Car 2</div> */}
-        </div>
-
-        <div className="mySlides fade">
-          <div className="numbertext">3 / 3</div>
-          <img
-            src="https://images.pexels.com/photos/810357/pexels-photo-810357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            style={{ width: '100%' }}
-            alt="Slide 3"
-          />
-          {/* <div className="text">Car 3</div> */}
-        </div>
-
-        <span className="prev" onClick={() => plusSlides(-1)}>prev</span>
-        <span className="next" onClick={() => plusSlides(1)}>next</span>
+      <div id="myBtnContainer">
+        <button className={filter === 'all' ? 'btn active' : 'btn'} onClick={() => handleFilter('all')}>
+          Show all
+        </button>
+        <button className={filter === 'nature' ? 'btn active' : 'btn'} onClick={() => handleFilter('nature')}>
+          Nature
+        </button>
+        <button className={filter === 'cars' ? 'btn active' : 'btn'} onClick={() => handleFilter('cars')}>
+          Cars
+        </button>
+        <button className={filter === 'people' ? 'btn active' : 'btn'} onClick={() => handleFilter('people')}>
+          People
+        </button>
       </div>
-    </>
+
+      <div className="row">
+        {portfolioItems.map((item, index) => (
+          <div key={index} className={`column ${item.category === filter || filter === 'all' ? 'show' : ''}`}>
+            <div className="content">
+              <img src={item.image} alt={item.alt} style={{ width: '100%' }} />
+              <h4>{item.title}</h4>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
+
+// Sample data for portfolio items
+const portfolioItems = [
+  { category: 'nature', image: '/images/mountain.jpg', alt: 'Mountains', title: 'Mountains', description: 'I love mountains..' },
+  { category: 'nature', image: '/images/lights.jpg', alt: 'Lights', title: 'Lights', description: 'I love nature lights..' },
+  { category: 'nature', image: '/images/forest.jpg', alt: 'Nature', title: 'Forest', description: 'I love Forest..' },
+  { category: 'cars', image: '/images/retrocar.jpg', alt: 'Car', title: 'Retro', description: 'I love retro car..' },
+  { category: 'cars', image: '/images/fastcar.jpg', alt: 'Car', title: 'Fast', description: 'I love race car..' },
+  { category: 'cars', image: '/images/classiccar.jpg', alt: 'Car', title: 'Classic', description: 'I love classic car..' },
+  { category: 'people', image: '/images/girl.webp', alt: 'girl', title: 'Girl', description: 'Respect girls..' },
+  { category: 'people', image: '/images/man.jpg', alt: 'man', title: 'Man', description: 'man will be man..' },
+  { category: 'people', image: '/images/woman.jpg', alt: 'woman', title: 'Woman', description: 'woman power..' },
+];
+
+export default Slider;
